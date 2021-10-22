0d8e7bfb59bcae1dee464a63cfa21038747a9e6b
sashadb <sashadbrown@gmail.com>
2021-10-22 16:00:49 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -25,7 +25,7 @@ $(window).on('load', function() {
     */
     
     return L.divIcon({
-    html: '<i class="circle"></i>',
+    html: '<i class="fa fa-circle-o"></i>',
     iconSize: [20, 20],
     className: 'myDivIcon'
     });
