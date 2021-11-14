4e1ed4a96b373e4957924c6860a703ed92207dcc
sashadb <sashadbrown@gmail.com>
2021-11-14 17:15:14 +0000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -129,7 +129,9 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description'], {maxWidth : 260});
+          point['Description'], 
+                     {maxWidth : 260},
+                     {minWidth : 260});
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
