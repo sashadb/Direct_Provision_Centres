99a18bdda6bdec9fb83d8ca1ef7349b2b417152e
sashadb <sashadbrown@gmail.com>
2021-11-14 17:17:50 +0000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,7 +130,7 @@ $(window).on('load', function() {
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description'], 
-                     {maxWidth : 260
+                     {maxWidth : 260,
                      minWidth : 260});
 
         if (layers !== undefined && layers.length !== 1) {
